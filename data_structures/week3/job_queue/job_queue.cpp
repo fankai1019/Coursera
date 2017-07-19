10bb64ebbfc5bb251a3674fd6ba6c65030e796de
fankai1019 <kai.fan.1019@gmail.com>
2017-07-18 21:12:17 -0400

    solved stack overflow problem and pass the test cases

--- data_structures/week3/job_queue/job_queue.cpp
+++ data_structures/week3/job_queue/job_queue.cpp
@@ -5,7 +5,6 @@
 #include <utility>
 #include <cstdlib>
 
-
 using std::vector;
 using std::cin;
 using std::cout;
@@ -15,10 +14,10 @@ class JobQueue {
 private:
     long long num_workers_;
     vector<long long> jobs_;
-
     vector<long long> assigned_workers_;
     vector<long long> start_times_;
     vector< pair<long long, long long> > end_times_;
+
 
     void siftup(long long i){
         while (i > 0 && (end_times_[(i-1)/2].first > end_times_[i].first || (end_times_[(i-1)/2].first == end_times_[i].first && end_times_[(i-1)/2].second > end_times_[i].second))){
@@ -30,10 +29,11 @@ private:
     void siftdown(long long i){
         long long minIndex = i;
         long long l=2*i + 1;
-        if((l < end_times_.size() && end_times_[l].first < end_times_[minIndex].first)||(end_times_[l].first == end_times_[minIndex].first && end_times_[l].second < end_times_[minIndex].second))
+        if((l < end_times_.size() && end_times_[l].first < end_times_[minIndex].first)||(l < end_times_.size()&&end_times_[l].first == end_times_[minIndex].first && end_times_[l].second < end_times_[minIndex].second))//no l<end_times_.size() will result in stack overflow
             minIndex = l;
         long long r = 2*i + 2;
-        if((r < end_times_.size() && end_times_[r].first < end_times_[minIndex].first)||(end_times_[r].first == end_times_[minIndex].first && end_times_[r].second < end_times_[minIndex].second))
+        if((r < end_times_.size() && end_times_[r].first < end_times_[minIndex].first)||(r < end_times_.size()&&end_times_[r].first == end_times_[minIndex].first && end_times_[r].second < end_times_[minIndex].second))//no l<end_times_.size() will result in stack overflow
+
             minIndex = r;
         if(i != minIndex){
             swap(end_times_[i], end_times_[minIndex]);
@@ -74,7 +74,7 @@ private:
         start_times_.resize(jobs_.size(),0);
         assigned_workers_.resize(jobs_.size(),0);
         for(long long i=0; i < jobs_.size(); ++i){
-           if(end_times_.size() != num_workers_){
+           if(end_times_.size() < num_workers_){
                assigned_workers_[i]=i;
                insert(pair<long long , long long> (start_times_[i]+jobs_[i],assigned_workers_[i]));
            }else{
@@ -85,46 +85,19 @@ private:
            }
 
         }
-//        assigned_workers_.resize(jobs_.size());
-//        start_times_.resize(jobs_.size());
-//        vector<long long> next_free_time(num_workers_, 0);
-//        for (long long i = 0; i < jobs_.size(); ++i) {
-//            long long duration = jobs_[i];
-//            long long next_worker = 0;
-//            for (long long j = 0; j < num_workers_; ++j) {
-//                if (next_free_time[j] < next_free_time[next_worker])
-//                next_worker = j;
-//            }
-//            assigned_workers_[i] = next_worker;
-//            start_times_[i] = next_free_time[next_worker];
-//            next_free_time[next_worker] += duration;
-//        }
-    }
+   }
 
 public:
     void Solve() {
-        test();
-//        ReadData();
+        ReadData();
         AssignJobs();
         WriteResponse();
-    }
-    void test(){
-            long long m = rand()%100000+1;
-            num_workers_=rand()%m +1;
-            jobs_.resize(m);
-            for(long long i = 0; i< m;++i)
-                jobs_[i]=(rand()%1000000000);
-            cerr<<m<<endl;
     }
 };
 
 int main() {
     std::ios_base::sync_with_stdio(false);
-    while(true){
-        JobQueue test;
-        test.Solve();
-    }
-//    JobQueue job_queue;
-//    job_queue.Solve();
+    JobQueue job_queue;
+    job_queue.Solve();
     return 0;
 }
